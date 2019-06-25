770fa955166cec1ea80f41cbdf32ef472bffdacd
Muhammad Musah <muhammad@Muhammads-Machine.local>
2019-06-25 14:15:12 -0400

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,15 @@
-class Model
-
+class Dog
+    attr_reader :name, :breed, :age
+    attr_writer :name, :age
+    @@all = []
+    def initialize(name, breed, age)
+        @name = name
+        @breed = breed
+        @age = age
+        @@all << self
+    end 
+    
+    def self.all
+        @@all
+    end
 end
